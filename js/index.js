7bb23755a7e7f0c0a56f520a0bdc1667bf6fe701
AlexandreJareck <alexandrejareck@gmail.com>
2022-03-10 21:36:07 -0300

    Add table restaurants

--- js/index.js
+++ js/index.js
@@ -1,41 +1,39 @@
-function login() {
+function login(e) {
+  e.preventDefault();
+  const email = document.querySelector("#email").value;
+  const pswd = document.querySelector("#password").value; // 123
+  const cryptoPswd = sha1(pswd);
 
-  debugger;
-    const email = document.querySelector("#email").value;
-    const pswd = document.querySelector("#password").value; // 123
-    const cryptoPswd = sha1(pswd);
+  console.log(email);
+  console.log(pswd);
+  console.log(cryptoPswd);
 
-    console.log(email);
-    console.log(pswd);
-    console.log(cryptoPswd);
+  const body = new URLSearchParams({
+    "grant_type": "password",
+    "username": email,
+    "password": pswd
+    // "password": cryptoPswd
+  });
 
-    const body = new URLSearchParams({
-        "grant_type": "password",
-        "username": email,
-        "password": pswd
-        // "password": cryptoPswd
+  fetch("http://localhost:8080/oauth/token", {
+    method: "POST",
+    headers: {
+      "Authorization": "Basic YWxleGFuZHJlOjEyMw==",
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: body
+  }).then(res => res.json())
+    .then(res => {
+      localStorage.setItem("access_token", res.access_token);
+      window.location.href = "/register.html"
     });
-
-    fetch("http://localhost:8080/oauth/token", {
-        method: "POST",
-        headers: {
-            "Authorization": "Basic YWxleGFuZHJlOjEyMw==",
-            "Content-type": "application/x-www-form-urlencoded",
-        },
-        body: body
-    }).then(res => res.json())
-      .then(res => {      
-        localStorage.setItem("access_token", res.access_token);
-        window.location.href = "/dashboard.html"
-        alert("123");
-      });
 
 }
 
-window.onload = function() {
-    
-    console.log("terminou de carregar a página...");
+window.onload = function () {
 
-    document.querySelector("#email").focus();
+  console.log("terminou de carregar a página...");
+
+  document.querySelector("#email").focus();
 
 }
---
+++ js/restaurants.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+
+  const accessToken = localStorage.getItem("access_token");
+
+  if (!accessToken) {
+    alert('Não autorizado')
+    return;
+  }
+
+  var data = [];
+debugger;
+  fetch("http://localhost:8080/restaurantes", {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+  })
+    .then(res => res.json())
+    .then(res => {
+      data = res;
+
+      var root = document.getElementById('root');
+      data.forEach(element => root.insertAdjacentHTML('beforebegin',
+        `<tr><td>${element.nome}</td><td>${element.taxaFrete}</td></tr>`));
+
+    });
+}
